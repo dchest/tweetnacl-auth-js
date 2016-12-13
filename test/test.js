207884ddd4502c2c7df2c1163f01257b921cb2bc
Dmitry Chestnykh <dmitry@codingrobots.com>
2016-12-13 12:20:16 +0100

    Use tweetnacl-util for test; use yarn

--- test/test.js
+++ test/test.js
@@ -1,18 +1,19 @@
 var auth = require('../' + (process.env.HMAC_SRC || 'nacl-auth.js'));
 var nacl = require('tweetnacl/nacl-fast');
+var util = require('tweetnacl-util');
 var test = require('tape');
 
 var vectors = require('./data/hmac.random');
 
 test('hmac random test vectors', function(t) {
   vectors.forEach(function(vec) {
-    var msg = nacl.util.decodeBase64(vec[0]);
-    var key = nacl.util.decodeBase64(vec[1]);
-    var goodMac = nacl.util.decodeBase64(vec[2]);
+    var msg = util.decodeBase64(vec[0]);
+    var key = util.decodeBase64(vec[1]);
+    var goodMac = util.decodeBase64(vec[2]);
     var mac = auth(msg, key);
-    t.equal(nacl.util.encodeBase64(mac), nacl.util.encodeBase64(goodMac.subarray(0, 32)));
+    t.equal(util.encodeBase64(mac), util.encodeBase64(goodMac.subarray(0, 32)));
     var fullMac = auth.full(msg, key);
-    t.equal(nacl.util.encodeBase64(fullMac), nacl.util.encodeBase64(goodMac));
+    t.equal(util.encodeBase64(fullMac), util.encodeBase64(goodMac));
   });
   t.end();
 });
